a31c8bc6a1ac06c5b69839035aaf0c1d7a8f6a8b
gsosarolon <gonzalososarolon@gmail.com>
2014-10-26 11:42:46 -0300

    cambios en asset loader para cargar sounds

--- AssetLoader.cpp
+++ AssetLoader.cpp
@@ -7,6 +7,7 @@
 
 #include "AssetLoader.h"
 #include "SpriteUtil.h"
+#include "SimpleAudioEngine.h"
 
 namespace dxco {
 
@@ -14,21 +15,33 @@ AssetLoader::AssetLoader() {
 	this->currentAsset = 0;
 }
 
-void AssetLoader::addAsset(std::string assetPath) {
-	this->assetsToLoad.push_back(assetPath);
+void AssetLoader::addAsset(std::string assetPath, bool isSound) {
+
+	if (isSound) {
+		this->soundAssetsToLoad.push_back(assetPath);
+	} else {
+		this->assetsToLoad.push_back(assetPath);
+	}
 }
 
 void AssetLoader::loadNext() {
 
 	if (this->hasNext()) {
-		std::string nextAsset = this->assetsToLoad[this->currentAsset];
-		dxco::SpriteUtil::preloadTextureWithFile(nextAsset.c_str());
-		this->currentAsset++;
+
+		if (this->currentAsset < this->assetsToLoad.size()) {
+			std::string nextAsset = this->assetsToLoad[this->currentAsset];
+			dxco::SpriteUtil::preloadTextureWithFile(nextAsset.c_str());
+			this->currentAsset++;
+		} else {
+			std::string nextAsset = this->assetsToLoad[this->currentAsset - this->assetsToLoad.size()];
+			CocosDenshion::SimpleAudioEngine::sharedEngine()->preloadEffect(nextAsset.c_str());
+			this->currentAsset++;
+		}
 	}
 }
 
 bool AssetLoader::hasNext() {
-	return this->currentAsset < this->assetsToLoad.size();
+	return this->currentAsset < this->assetsToLoad.size() + this->soundAssetsToLoad.size();
 }
 
 } /* namespace dxco */
--- AssetLoader.h
+++ AssetLoader.h
@@ -16,11 +16,12 @@ namespace dxco {
 class AssetLoader {
 public:
 	AssetLoader();
-	void addAsset(std::string assetPath);
+	void addAsset(std::string assetPath, bool isSound=false);
 	void loadNext();
 	bool hasNext();
 private:
 	std::vector<std::string> assetsToLoad;
+	std::vector<std::string> soundAssetsToLoad;
 	int currentAsset;
 };
 
