e04f576fee0e9c7690a7f75bfcb35533edb2c6f9
Facundo Olano <facundo.olano@gmail.com>
2015-05-22 14:31:47 -0300

    added format method to string utils

--- StringUtil.h
+++ StringUtil.h
@@ -15,6 +15,9 @@ public:
 	static std::string padLeft(std::string &str, const int stringSize, const char paddingChar = ' ');
 	static std::string padLeft(int number, const int stringSize);
 	static std::string intToKString(int number);
+
+	static std::string sFormat(std::string s, std::string value);
+	static std::string iFormat(std::string s, int value);
 };
 
 } /* namespace dxco */
